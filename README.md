71f92a42bc6db6610e3ed68d42ba5be6c4b3db83
CinthiaNT <cinthya_230697@hotmail.com>
2018-09-27 01:16:23 -0500

    Sistema Notificador V2

--- README.md
+++ README.md
@@ -1,36 +1,63 @@
-﻿PRACTICA #2 25 de Septiembre del 2018
+﻿PRACTICA #2 
+25 de Septiembre del 2018
+
 
 
 "Sistema Notificador de Llegada de Individuos"
 
 
 
+
+
 Alarma usando arduino para el acceso a una puerta.
+
 
 
 La puerta se abre cuando detecta que un individuo esta cerca y se emite una alarma 
 que emite Luz y sonido.
+
 La simulacion del inidviduo es con la ayuda de un imán y la puerta con un sensor magnetico.
 
-La energia del arduino se debe reducir al maximo cuando no este cerca ningun individuo.
 
+La energia del arduino se debe reducir al maximo cuando no este cerca ningun individuo.
+ 
+
+FUNCIONALIDAD EXTRA 
+Se proporciona la opcion de que el administrador de la alarma puede desactivarla manualmente
+en caso de que el sensor quede obstruido por alguna accion NO peligrosa.
+
+
+
+HERRAMIENTAS DE DESARROLLO
+* Arduino IDE
+
+LIBRERIAS
+*Low-Power-Master
 
 
 MATERIAL
 
+
 *1 Arduino uno
+
 
 *1 LED
 
+
 *1 Red Switch
+
 
 *1 Resistencia de 220 Ohms
 
+
 *1 Buzzer
+
 
 *1 Protoboard
 
 
+
+*1 Boton
+*Jumpers
 ![Imagen](https://github.com/CinthiaNT/SistemaNotificador/blob/master/2.jpg)
 ![Imagen](https://github.com/CinthiaNT/SistemaNotificador/blob/master/1.jpg)
-
--- SistemaNotificador.ino
+++ SistemaNotificador.ino
@@ -1,45 +1,99 @@
-/*
+
+/* INSTITUTO TECNOLOGICO DE LEON
+ * INGENIERIA EN SISTEMAS COMPUTACIONALES 
+ * SISTEMAS PROGRAMABLES MARTES Y JUEVES 7:00- 8:40
+ * SEMESTRE AGOSTO - DICIEMBRE 2018
+ * Fecha entrega: 27 DE SEPTIEMBRE DEL 2018
+ * NAVA TORRES JUANA CINTHIA LIZBETH
+ * 
   Sistema que simula una alarma con luz y sonido cuando una puerta se abre.
    La puerta se abre cuando un iman esta cerca
 */
-
+#include "LowPower.h"  // Se incluye la libreria LowPower que nos sera de ayuda para el bajo consumo de la placa arduino
 #define pin_Sensor 2   // Pin digital que alimenta al Sensor 
-#define pin_LED 4      // Pin digital que alimenta el LED que simula la alarma al abrir la puerta 
+#define pin_LED    4   // Pin digital que alimenta el LED que simula la alarma al abrir la puerta 
+#define pin_Boton  7   // Pin digital que alimenta al Boton que servira para apagar la alarma 
 #define pin_Buzzer 9   // Pin digital que alimenta el Buzzer que emite sonido cuando se abre la puerta
 
-// Arreglo que contiene las distintas frecuencias que se aproximan a las distintas notas musicales
-const int notas[] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494};
-// Contador para controlar el ciclo for que recorre el arreglo de notas
-const int contador_Notas = 10;
+
+  int valor_Sensor = 0; // Creamos una variable donde se guarda el valor que vaya tomando sensor
+  int length = 15; // Tamaño del arreglo de beats
+  char notes[] = "ccggaagffeeddc "; // Arreglo de caracteres que nos servira para el sonido de la alarma
+  int beats[] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 4 }; 
+  int tempo = 300;
 
 void setup() {
-  //Se configura pin como entrada con resistencia pull-up interna para sensor magnetico
-  pinMode(pin_Sensor, INPUT_PULLUP);
-  // Se  configuran los pines de Led y Buzzer como pines de salida
-  pinMode(pin_LED, OUTPUT);
-  pinMode(pin_Buzzer, OUTPUT);
+  pinMode(pin_Sensor, INPUT_PULLUP);//Se configura pin como entrada con resistencia pull-up interna para sensor magnetico
+  pinMode(pin_Boton, INPUT); // Se configura pin de entrada el pin correspondiente al Boton
+  pinMode(pin_LED, OUTPUT);  // Se  configura pin de salida el pin corresponidnete al LED
+  pinMode(pin_Buzzer, OUTPUT); // Se  configura pin de salida el pin corresponidnete al Buzzer
+  
+  attachInterrupt(0, valorSensor, FALLING); // Interrupcion por default el puerto es el 2. En este codigo es el pin
+                                            // correspondiente al reed switch y nos dirige al metodo valorSensor
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); // Se hace uso de la libreria de bajo consumo para que arduino entre
+                                                       // en modo sleep()
+  detachInterrupt(0); // Se rompe con la interrupcion
 }
 
 void loop() {
-  //Se guarda en una variable llamada valor lo que nos regrese el pin que alimenta al Sensor magnetico
-  int valor = digitalRead(pin_Sensor);
-  //Se usa un condicional para activar la alarma
-  //Si el sensor magnetico detecta el iman cerca regresa como valor LOW y se procede a encender el LED
-  //y Buzzer 
-  if (valor == LOW) {
-    digitalWrite(pin_LED, HIGH);
-    //Ciclo for que recorre el arreglo de notas
-    for (int iTono = 0; iTono < contador_Notas; iTono++)
-    {
-      tone(pin_Buzzer, notas[iTono]);
-      delay(1000);
+  // Se entra a un ciclo for cuando el valor del sensor es LOW esto significa que un iman activo nuestro sensor
+  // y se activa alarma que emite Luz y sonido
+  while (valor_Sensor == LOW) {
+    //Con el ciclo for generamos una serie de tonos que seran nuestra alarma
+    for (int i = 0; i < length; i++) {
+      if (notes[i] == ' ') {
+        delay(beats[i] * tempo);
+      } else {
+        playNote(notes[i], beats[i] * tempo);
+      }
+
+      // pausa entre notas
+      delay(tempo / 4);
     }
-    noTone(pin_Buzzer);
-  } else {
-    //Cuando el sensor magnetico no detecta un iman entonces el LED y Buzzer permanecen apagados
-    digitalWrite(pin_LED, LOW);
-    digitalWrite(pin_Buzzer, LOW);
+    // Condicion que verifica si el boton para desactivar la alarma este presionado
+    // De ser asi, el valor de sensor ccambiara a HIGH lo que nos hara romper con el ciclo
+    // Aun cuando el iman siga en nuestro sensor magnetico. De lo contrario el ciclo se rompera 
+    // siempre y cuando el iman este lejos. 
+    if (digitalRead(pin_Boton) == HIGH) {
+      valor_Sensor = HIGH;
+    } else {
+      valor_Sensor = digitalRead(pin_Sensor);
+    }
   }
-  //Con DelayMicroseconds gastamos consumimos menos energia que al usar delay
-  delayMicroseconds(500);
+  // Al salir del ciclo while ponemos nuevamente la placa de arduino en modo sleep para que consuma el minimo
+  // de energia
+  attachInterrupt(0, valorSensor, FALLING);
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+}
+
+
+void valorSensor() {
+  // Con este metodo leemos el valor del sensor
+  valor_Sensor = digitalRead(pin_Sensor);
+}
+
+// Metodo para generar la serie de tonos para alarma 
+void playTone(int tone, int duration) {
+  //metodo para calcular los tonos de la melodia de la bocina
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(pin_LED, HIGH);//apaga el led
+    digitalWrite(pin_Buzzer, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(pin_LED, LOW);//enciende el led
+    digitalWrite(pin_Buzzer, LOW);
+    delayMicroseconds(tone);
+  }
+}
+// Metodo para generar la serie de tonos para alarma 
+void playNote(char note, int duration) {
+  //metodo para la secuencia de las notas de la melodia
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+
+  // toca el tono correspondiente al nombre de la nota
+  for (int i = 0; i < 2; i++) {
+    if (names[i] == note) {
+      playTone(tones[i], duration);
+    }
+  }
 }
