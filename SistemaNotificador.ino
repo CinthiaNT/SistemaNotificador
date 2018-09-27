51bb86b7003232577c63b595481734e1ba5fed54
CinthiaNT <cinthya_230697@hotmail.com>
2018-09-27 08:37:21 -0500

    Actualizacion Version 3

--- SistemaNotificador.ino
+++ SistemaNotificador.ino
@@ -1,4 +1,3 @@
-
 /* INSTITUTO TECNOLOGICO DE LEON
  * INGENIERIA EN SISTEMAS COMPUTACIONALES 
  * SISTEMAS PROGRAMABLES MARTES Y JUEVES 7:00- 8:40
@@ -17,10 +16,10 @@
 
 
   int valor_Sensor = 0; // Creamos una variable donde se guarda el valor que vaya tomando sensor
-  int length = 15; // Tamaño del arreglo de beats
-  char notes[] = "ccggaagffeeddc "; // Arreglo de caracteres que nos servira para el sonido de la alarma
-  int beats[] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 4 }; 
-  int tempo = 300;
+  int length = 26;
+char notes[] = "eeeeeeegcde fffffeeeeddedg";
+int beats[] = { 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2};
+int tempo = 150;
 
 void setup() {
   pinMode(pin_Sensor, INPUT_PULLUP);//Se configura pin como entrada con resistencia pull-up interna para sensor magnetico
@@ -32,33 +31,30 @@ void setup() {
                                             // correspondiente al reed switch y nos dirige al metodo valorSensor
   LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); // Se hace uso de la libreria de bajo consumo para que arduino entre
                                                        // en modo sleep()
-  detachInterrupt(0); // Se rompe con la interrupcion
+ 
 }
 
 void loop() {
+   detachInterrupt(0); // Se rompe con la interrupcion
   // Se entra a un ciclo for cuando el valor del sensor es LOW esto significa que un iman activo nuestro sensor
   // y se activa alarma que emite Luz y sonido
   while (valor_Sensor == LOW) {
     //Con el ciclo for generamos una serie de tonos que seran nuestra alarma
-    for (int i = 0; i < length; i++) {
-      if (notes[i] == ' ') {
-        delay(beats[i] * tempo);
-      } else {
-        playNote(notes[i], beats[i] * tempo);
-      }
-
-      // pausa entre notas
-      delay(tempo / 4);
+     for (int i = 0; i < length; i++) {
+    if (notes[i] == ' ') {
+      delay(beats[i] * tempo);
+    } else {
+      playNote(notes[i], beats[i] * tempo);
     }
+    //Pausa entre las notas
+    delay(tempo / 2); 
+  }
+  valor_Sensor = digitalRead(pin_Sensor);
     // Condicion que verifica si el boton para desactivar la alarma este presionado
     // De ser asi, el valor de sensor ccambiara a HIGH lo que nos hara romper con el ciclo
     // Aun cuando el iman siga en nuestro sensor magnetico. De lo contrario el ciclo se rompera 
     // siempre y cuando el iman este lejos. 
-    if (digitalRead(pin_Boton) == HIGH) {
-      valor_Sensor = HIGH;
-    } else {
-      valor_Sensor = digitalRead(pin_Sensor);
-    }
+   
   }
   // Al salir del ciclo while ponemos nuevamente la placa de arduino en modo sleep para que consuma el minimo
   // de energia
@@ -74,24 +70,32 @@ void valorSensor() {
 
 // Metodo para generar la serie de tonos para alarma 
 void playTone(int tone, int duration) {
-  //metodo para calcular los tonos de la melodia de la bocina
-  for (long i = 0; i < duration * 1000L; i += tone * 2) {
-    digitalWrite(pin_LED, HIGH);//apaga el led
-    digitalWrite(pin_Buzzer, HIGH);
-    delayMicroseconds(tone);
-    digitalWrite(pin_LED, LOW);//enciende el led
-    digitalWrite(pin_Buzzer, LOW);
-    delayMicroseconds(tone);
+  if (digitalRead(pin_Boton) == LOW) {
+    valor_Sensor = HIGH;
+    attachInterrupt(0, valorSensor, FALLING);
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+  }else{
+    
+  
+  valor_Sensor = digitalRead(pin_Sensor);
+  if( valor_Sensor == LOW){
+    for (long i = 0; i < duration * 1000L; i += tone * 2) {
+      digitalWrite(pin_LED, HIGH);
+      digitalWrite(pin_Buzzer, HIGH);
+      delayMicroseconds(tone);
+      digitalWrite(pin_LED, LOW);
+      digitalWrite(pin_Buzzer, LOW);
+      delayMicroseconds(tone);    
+    }
+   }
   }
 }
-// Metodo para generar la serie de tonos para alarma 
+
 void playNote(char note, int duration) {
-  //metodo para la secuencia de las notas de la melodia
   char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
   int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
-
-  // toca el tono correspondiente al nombre de la nota
-  for (int i = 0; i < 2; i++) {
+ 
+  for (int i = 0; i < 8; i++) {
     if (names[i] == note) {
       playTone(tones[i], duration);
     }
